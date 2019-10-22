a12c20ab095acfffadbc36f45fdd420914d976cb
muratvuranok <muratvuranok@hotmail.com>
2019-10-22 12:06:48 +0300

    Değişkenler bitti

---
+++ Degiskenler/Form2.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Degiskenler
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTamSayilar = new System.Windows.Forms.Button();
+            this.btnOndalikliSayilar = new System.Windows.Forms.Button();
+            this.btnMetinsel = new System.Windows.Forms.Button();
+            this.btnMantiksal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTamSayilar
+            // 
+            this.btnTamSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTamSayilar.Location = new System.Drawing.Point(12, 12);
+            this.btnTamSayilar.Name = "btnTamSayilar";
+            this.btnTamSayilar.Size = new System.Drawing.Size(302, 77);
+            this.btnTamSayilar.TabIndex = 0;
+            this.btnTamSayilar.Text = "Tam Sayılar";
+            this.btnTamSayilar.UseVisualStyleBackColor = true;
+            this.btnTamSayilar.Click += new System.EventHandler(this.btnTamSayilar_Click);
+            // 
+            // btnOndalikliSayilar
+            // 
+            this.btnOndalikliSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOndalikliSayilar.Location = new System.Drawing.Point(12, 95);
+            this.btnOndalikliSayilar.Name = "btnOndalikliSayilar";
+            this.btnOndalikliSayilar.Size = new System.Drawing.Size(302, 77);
+            this.btnOndalikliSayilar.TabIndex = 1;
+            this.btnOndalikliSayilar.Text = "Ondalıklı Sayılar";
+            this.btnOndalikliSayilar.UseVisualStyleBackColor = true;
+            this.btnOndalikliSayilar.Click += new System.EventHandler(this.btnOndalikliSayilar_Click);
+            // 
+            // btnMetinsel
+            // 
+            this.btnMetinsel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMetinsel.Location = new System.Drawing.Point(12, 178);
+            this.btnMetinsel.Name = "btnMetinsel";
+            this.btnMetinsel.Size = new System.Drawing.Size(302, 77);
+            this.btnMetinsel.TabIndex = 2;
+            this.btnMetinsel.Text = "Metinsel";
+            this.btnMetinsel.UseVisualStyleBackColor = true;
+            this.btnMetinsel.Click += new System.EventHandler(this.btnMetinsel_Click);
+            // 
+            // btnMantiksal
+            // 
+            this.btnMantiksal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMantiksal.Location = new System.Drawing.Point(12, 261);
+            this.btnMantiksal.Name = "btnMantiksal";
+            this.btnMantiksal.Size = new System.Drawing.Size(302, 77);
+            this.btnMantiksal.TabIndex = 3;
+            this.btnMantiksal.Text = "Mantıksal";
+            this.btnMantiksal.UseVisualStyleBackColor = true;
+            this.btnMantiksal.Click += new System.EventHandler(this.btnMantiksal_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 354);
+            this.Controls.Add(this.btnMantiksal);
+            this.Controls.Add(this.btnMetinsel);
+            this.Controls.Add(this.btnOndalikliSayilar);
+            this.Controls.Add(this.btnTamSayilar);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTamSayilar;
+        private System.Windows.Forms.Button btnOndalikliSayilar;
+        private System.Windows.Forms.Button btnMetinsel;
+        private System.Windows.Forms.Button btnMantiksal;
+    }
+}
---
+++ Degiskenler/Form2.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Degiskenler
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        /*
+   Değişken Nedir?       
+   Değişkenleri en sabit şekilde verilerin depolandğı yerler olarak tanımlayabiliriz. Değişkenlerin içine verilerimizi koyabilirirz veya değişkenlerimizin içindeki verileri C# programındaki işlemlerimiz için kullanabilliriz
+ */
+
+        /*
+         Degisken Kuralları
+         
+         1)Kullanmayacagimiz degiskenleri asla tanimlamamaliyiz...
+         2)Deger araliklarina gore dogru veri tiplerini secmeye ozen gostermeliyiz... (Degisken Yonetimi = Performans Yonetimi)
+         3)Bir degisken adi asla iki kelimeden olusamaz... Eger iki kelime ise mutlaka izin verilen ozel bir karakterle (_) birbirinden ayrilmalidir...
+         4)Degisken adlari asla sayiyla baslamaz ancak sayiyla bitebilir...
+        */
+
+
+        private void btnTamSayilar_Click(object sender, EventArgs e)
+        {
+            // Ctrl + R + G => gereksiz using'leri siler
+            // Ctrl + K + D => kodları düzenleme
+            // Ctrl + E + W => Wordwrap
+            // Ctrl + K + C => yorum satırına alma
+            // Ctrl + K + U => yorum satırından çıkartma
+
+
+            // byte => ram üzerinde 8 bit'lik bir alan kapsar
+            // MaxValue = 255;
+            // MinValue = 0;
+
+            byte sayi1 = 12;
+
+            // tanımlanan nesne'nin altı yeşil çizili ise, daha önce kullanılmadığına işaret eder, bu bizim için performans kaybıdır. kullanmayacağımız nesneleri, yorum satırına alınız ya da siliniz.      
+
+            // sbyte => ram üzerinde 8 bit'lik bir alan kapsar
+            // MaxValue = 127;
+            // MinValue = -128;
+            sbyte sayi2 = 23;
+
+
+            // short => ram üzerinde 16 bit'lik bir alan kapsar
+            // MaxValue = 32767;
+            // MinValue = -32768;
+            short sayi3 = -213;
+
+
+            // ushort => ram üzerinde 16 bit'lik bir alan kapsar
+            // UInt16 MaxValue = 65535;
+            // UInt16 MinValue = 0;
+            ushort sayi4 = 123;
+
+
+            // int  => ram üzerinde 32 bit'lik bir alan kapsar
+            // MaxValue = 2147483647;
+            // MinValue = -2147483648;
+            int sayi5 = 21321;
+
+
+            // uint => ram üzerinde 32 bit'lik bir alan kapsar
+            // MaxValue = 4294967295;
+            // MinValue = 0;
+            uint sayi6 = 2131;
+
+            // long => ram üzerinde 64 bit'lik bir alan kapsar
+            //  MaxValue = 9223372036854775807;
+            //  MinValue = -9223372036854775808;
+            long sayi7 = 2312321;
+
+            // ulong => ram üzerinde 64 bit'lik bir alan kapsar
+            // MaxValue = 18446744073709551615;
+            // MinValue = 0;
+
+            ulong sayi8 = 213213;
+        }
+
+        private void btnOndalikliSayilar_Click(object sender, EventArgs e)
+        {
+            // float => ram üzerinde 32 bit'lik bir alan kapsar
+            // MinValue = -3.40282347E+38F;
+            // Epsilon = 1.401298E-45F;
+            // MaxValue = 3.40282347E+38F;
+            // PositiveInfinity = 1F / 0F;
+            // NegativeInfinity = -1F / 0F;
+            // NaN = 0F / 0F;
+
+            float sayi1 = 213.1231F;
+
+
+            // MinValue = -1.7976931348623157E+308;
+            // MaxValue = 1.7976931348623157E+308;
+            // Epsilon = 4.94065645841247E-324;
+            // NegativeInfinity = -1D / 0D;
+            // PositiveInfinity = 1D / 0D;
+            // NaN = 0D / 0D;
+
+            double sayi2 = 23213.213213;
+
+
+
+            // Zero = 0;
+            // One = 1;
+            // MinusOne = -1;
+            // MaxValue = 79228162514264337593543950335M;
+            // MinValue = -79228162514264337593543950335M;
+            decimal sayi3 = 213.123213213M;
+        }
+
+        private void btnMetinsel_Click(object sender, EventArgs e)
+        {
+            // char => ram üzerinde 16 bit'lik bir alan kapsar 
+            //MaxValue = '\uffff';
+            //MinValue = '\0';
+
+            char karakter = 'A';
+            string metin = "bilge \"{adam}\" beşiktaş";
+
+            //MessageBox.Show(metin);
+            //MessageBox.Show("bilge adam beşiktaş yazılım dersleri");
+
+            // bilge "adam" beşiktaş
+            metin = "bilge\nadam\nbeşiktaş";  // \n => new line (yeni bir satır)
+
+            //bilge
+            //adam
+            //beşiktaş
+            metin = "bilge\tadam\tbeşiktaş";
+            MessageBox.Show(metin);
+        }
+
+        private void btnMantiksal_Click(object sender, EventArgs e)
+        {
+            bool olumlu = true;
+            bool olumsuz = false;
+
+            bool result1 = 10 > 4; // => true
+            bool result2 = 10 < 4; // => false 
+        }
+    }
+}
